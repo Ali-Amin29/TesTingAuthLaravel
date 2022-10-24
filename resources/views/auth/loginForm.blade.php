af94b0557edfe5c7789594b410654624d7cd762e
Ali-Amin29 <ali.amin2910@gmail.com>
2022-10-24 09:47:34 +0200

    Make my LoginForm

---
+++ resources/views/auth/loginForm.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+</head>
+<body>
+    <div class="text-center">
+        <div class="row mt-5 col-12">
+            <form method="POST" action="{{ route('login') }}">
+                @csrf
+            <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">Email address</label>
+            <input type="email" class="form-control" name="email" id="exampleInputEmail1"
+                value="{{  old('email')? old('email') :  'welcom Enter Email' }}" />
+            @error('email')
+                <span>{{ $message }}</span>
+            @enderror
+            </div>
+            <div class="mb-3">
+            <label for="exampleInputPassword1" class="form-label">Password</label>
+            <input type="password" class="form-control" name="password" id="exampleInputPassword1">
+            @error('password')
+            <span>{{ $message }}</span>
+            @enderror
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+        </div>
+    </div>
+
+</body>
+</html>
